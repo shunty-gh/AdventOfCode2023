b9bdf2a79bd82e9ed709da97e4405a6559790288
Steven Hunt <sph@shunty.co.uk>
2023-12-01 17:40:59 +0000

    Day 1, py v3

--- py/day01.py
+++ py/day01.py
@@ -14,33 +14,19 @@ for s in input:
 print("Part 1:", sum1)
 
 # Part 2
-digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 sum2 = 0
 for s in input:
+    nums = []
     i = 0
-    first = -1
-    last = -1
-    # Find first, scan forward
-    while i < len(s):
-        for idx, digit in enumerate(digits):
-            if s[i:].startswith(digit):
-                first = 10 * (idx + 1 if len(digit) > 1 else int(digit))
-                break
-        if first > 0:
-            break
-        i += 1
-
-    # Find last, scan backward
-    i = len(s) - 1
-    while i >= 0:
-        for idx, digit in enumerate(digits):
-            if s[i:].startswith(digit):
-                last = idx + 1 if len(digit) > 1 else int(digit)
-                break
-        if last >= 0:
-            break
-        i -= 1
-
-    sum2 += first + last
+    for i,c in enumerate(s):
+        if c.isdigit():
+            nums.append(int(c))
+        else:
+            for idx, digit in enumerate(digits):
+                if s[i:].startswith(digit):
+                    nums.append(idx + 1)
+                    break
+    sum2 += nums[0] * 10 + nums[-1]
 
 print("Part 2:", sum2)
