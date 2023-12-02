4a765ea489151821bd69398b45d3b37bd5433706
Steven Hunt <sph@shunty.co.uk>
2023-12-02 09:10:16 +0000

    Day 2, C#

--- cs/days/day02.cs
+++ cs/days/day02.cs
@@ -1,8 +1,9 @@
 using System.Collections.ObjectModel;
+using Spectre.Console.Rendering;
 
 namespace Shunty.AoC;
 
-// https://adventofcode.com/2023/day/2
+// https://adventofcode.com/2023/day/2 - Cube Conundrum
 
 public class Day02 : AocDaySolver
 {
@@ -11,7 +12,6 @@ public class Day02 : AocDaySolver
     public async Task Solve()
     {
         var input = await AocUtils.GetDayLines(DayNumber);
-        var p1 = 0;
         var games = new List<Game>();
         foreach (var s in input)
         {
@@ -21,57 +21,33 @@ public class Day02 : AocDaySolver
             var rounds = new List<GameRound>();
             foreach (var p in roundparts)
             {
-                int r = 0, g = 0, b = 0;
                 var rgbparts = p.Split(',');
                 foreach (var rgb in rgbparts)
                 {
                     var split = rgb.Trim().Split(' ');
                     var num = int.Parse(split[0]);
-                    switch (split[1].Trim())
-                    {
-                        case "red":
-                            r = num;
-                            break;
-                        case "green":
-                            g = num;
-                            break;
-                        case "blue":
-                            b = num;
-                            break;
-                    }
+                    var r = split[1] == "red" ? num : 0;
+                    var g = split[1] == "green" ? num : 0;
+                    var b = split[1] == "blue" ? num : 0;
                     rounds.Add(new(r,g,b));
                 }
             }
-            var game = new Game { GameId = gameId, Rounds = rounds };
-            games.Add(game);
+            games.Add(new(gameId, rounds));
         }
 
         int mred = 12, mgreen = 13, mblue = 14;
-        foreach (var game in games)
-        {
-            if (game.Rounds.All(r => r.Red <= mred && r.Green <= mgreen && r.Blue <= mblue))
-            {
-                p1 += game.GameId;
-            }
-        }
+        var p1 = games
+            .Where(g => g.Rounds.All(r => r.Red <= mred && r.Green <= mgreen && r.Blue <= mblue))
+            .Sum(g => g.GameId);
         this.ShowDayResult(1, p1);
+
+        var p2 = games.Sum(g =>
+            g.Rounds.Max(r => r.Red)
+            * g.Rounds.Max(r => r.Green)
+            * g.Rounds.Max(r => r.Blue));
+        this.ShowDayResult(2, p2);
     }
 }
 
-public struct GameRound
-{
-    public GameRound(int r, int g, int b)
-    {
-        Red = r;
-        Green = g;
-        Blue = b;
-    }
-    public int Red { get; init; }
-    public int Green { get; init; }
-    public int Blue { get; init; }
-}
-public record struct Game
-{
-    public int GameId { get; set; }
-    public List<GameRound> Rounds { get; set; }
-}
+public record GameRound(int Red, int Green, int Blue);
+public  record Game(int GameId, ICollection<GameRound> Rounds);
