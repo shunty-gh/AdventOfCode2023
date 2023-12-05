304723848c401ffd50c196dacbddec8d00999443
Steven Hunt <sph@shunty.co.uk>
2023-12-05 12:22:12 +0000

    Day 5, C# - part 2, brute force and ignorance (and so slow)

--- cs/days/day05.cs
+++ cs/days/day05.cs
@@ -1,6 +1,12 @@
 namespace Shunty.AoC;
 
-// https://adventofcode.com/2023/day/5
+// https://adventofcode.com/2023/day/5 - If You Give A Seed A Fertilizer
+
+/// The *very* slow, brute force solution. About 20 minutes on my machine.
+/// But it does, at least, give the right answer.
+/// There is, obviously, a way faster solution somewhere but I just haven't
+/// quite discovered it yet.
+
 
 public class Day05 : AocDaySolver
 {
@@ -8,9 +14,10 @@ public class Day05 : AocDaySolver
 
     public async Task Solve()
     {
-        var maps = new Dictionary<string, List<AlmanacMapItem>>();
         var input = await AocUtils.GetDayLines(DayNumber);
-        var seeds = input[0].Split(':')[1].Trim().Split(' ').Select(s => Int64.Parse(s));
+
+        var seeds = input[0].Split(':')[1].Trim().Split(' ').Select(s => Int64.Parse(s)).ToList();
+        var maps = new Dictionary<string, List<AlmanacMapItem>>();
         var currentMap = "";
         foreach (var line in input.Skip(2))
         {
@@ -18,40 +25,87 @@ public class Day05 : AocDaySolver
                 continue;
             if (line.Trim().EndsWith("map:"))
             {
-                currentMap = line.Substring(0, line.Length - 5);
-                maps.Add(currentMap, new());
+                currentMap = line[..^5];
+                maps.Add(currentMap, []);
             }
             else
             {
                 var mapsplit = line.Split(' ').Select(s => Int64.Parse(s)).ToList();
-                maps[currentMap].Add(new(mapsplit[0], mapsplit[0] + mapsplit[2] - 1, mapsplit[1], mapsplit[1] + mapsplit[2] - 1, mapsplit[2]));
+                maps[currentMap].Add(
+                    new(mapsplit[0],
+                    mapsplit[0] + mapsplit[2] - 1,
+                    mapsplit[1],
+                    mapsplit[1] + mapsplit[2] - 1,
+                    mapsplit[2]));
             }
         }
 
-        var locations = new Dictionary<Int64, Int64>();
+        // Part 1
+        Int64 p1 = -1;
         foreach (var seed in seeds)
         {
-            // Seed to soil
-            var soil = GetDestination(seed, maps["seed-to-soil"]);
-            // Soil to fertilizer
-            var fert = GetDestination(soil, maps["soil-to-fertilizer"]);
-            // Fertilizer to water
-            var water = GetDestination(fert, maps["fertilizer-to-water"]);
-            // Water to light
-            var light = GetDestination(water, maps["water-to-light"]);
-            // Light to temperature
-            var temp = GetDestination(light, maps["light-to-temperature"]);
-            // Temperature to humidity
-            var humid = GetDestination(temp, maps["temperature-to-humidity"]);
-            // Humidity to location
-            var loc = GetDestination(humid, maps["humidity-to-location"]);
-            locations.Add(seed, loc);
+            var loc = GetLocationForSeed(seed, maps);
+            if (p1 < 0 || p1 > loc)
+                p1 = loc;
         }
-
-        var p1 = locations.Values.Min();
-        var p2 = 0;
         this.ShowDayResult(1, p1);
+
+        // Part 2
+        Int64 p2 = -1;
+        int seedid = 0;
+        var locker = new object();
+        var tasks = new List<Task>();
+        while (seedid < seeds.Count)
+        {
+            Int64 seedbase = seeds[seedid];
+            Int64 seedrange = seeds[seedid + 1];
+
+            var t = Task.Factory.StartNew(() => {
+                var lowest = GetLocationForSeedRange(seedbase, seedrange, maps.AsReadOnly());
+                lock(locker)
+                {
+                    if (p2 < 0 || p2 > lowest)
+                        p2 = lowest;
+                }
+            });
+            tasks.Add(t);
+
+            await Task.WhenAll(tasks);
+            seedid += 2;
+        }
         this.ShowDayResult(2, p2);
+    }
+
+    private Int64 GetLocationForSeedRange(Int64 seedBase, Int64 seedRange, IReadOnlyDictionary<string, List<AlmanacMapItem>> maps)
+    {
+        Int64 result = -1;
+
+        for (var ds = 0; ds < seedRange; ds++)
+        {
+            var seed = seedBase + ds;
+            var loc = GetLocationForSeed(seed, maps);
+            if (result == -1 || loc < result)
+                result = loc;
+        }
+        return result;
+    }
+
+    private Int64 GetLocationForSeed(Int64 seed, IReadOnlyDictionary<string, List<AlmanacMapItem>> maps)
+    {
+        // Seed to soil
+        var soil = GetDestination(seed, maps["seed-to-soil"]);
+        // Soil to fertilizer
+        var fert = GetDestination(soil, maps["soil-to-fertilizer"]);
+        // Fertilizer to water
+        var water = GetDestination(fert, maps["fertilizer-to-water"]);
+        // Water to light
+        var light = GetDestination(water, maps["water-to-light"]);
+        // Light to temperature
+        var temp = GetDestination(light, maps["light-to-temperature"]);
+        // Temperature to humidity
+        var humid = GetDestination(temp, maps["temperature-to-humidity"]);
+        // Humidity to location
+        return GetDestination(humid, maps["humidity-to-location"]);
     }
 
     private Int64 GetDestination(Int64 source, List<AlmanacMapItem> map)
